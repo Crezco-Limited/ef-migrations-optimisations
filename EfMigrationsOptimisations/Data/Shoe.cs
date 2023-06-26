67a70aa7e1bf136b893c470ee0eaea028137f4b8
Phil Broderick <Phil.Broderick@crezco.com>
2023-06-26 14:52:08 +0100

    Initial commit to test

---
+++ EfMigrationsOptimisations/Data/Shoe.cs
@@ -0,0 +1,8 @@
+﻿namespace EfMigrationsOptimisations.Data;
+
+public class Shoe
+{
+    public int Id { get; set; }
+
+    public string Make { get; set; } = default!;
+}
---
+++ EfMigrationsOptimisations/Data/TestContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EfMigrationsOptimisations.Data;
+
+public class TestContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Test");
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    public DbSet<Shoe> Shoes { get; set; }
+}
